ed9e2e9d1f13ed661ce7d094e2e2f9ddf338ac06
GTyexing <w1404278166@gmail.com>
2016-09-22 19:11:33 +0800

    Basic environment

---
+++ front-end/README.md
@@ -0,0 +1,13 @@
+# AutoPenParperSystem
+Interface of system that can make you make a *Examination paper* easy.
+
+
+## Running
+
+First `npm install` to grab all the necessary dependencies.
+
+Then run `npm start` and open <localhost:8888> in your browser.
+
+## Production Build
+
+Run `npm build` to create a distro folder and a bundle.js file.
--- front-end/client/autogenpapersystem.js
+++ front-end/client/autogenpapersystem.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+//import components
 import Main from './components/Main'
 
-import { Router, Route, IndexRoute, browserHistory} from 'react-router';
+//import react-router
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 const router = (
     <Router history={ browserHistory }>
         <Route path="/" component={Main} />
     </Router>
-)
+);
 
-render(router, document.getElementById('root'))
+render(router, document.getElementById('root'));
