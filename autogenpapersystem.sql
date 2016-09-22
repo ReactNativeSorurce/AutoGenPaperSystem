fb0e7c2d85afb25acd525ee51dbf3428a09103f1
sgt110 <1067541968@qq.com>
2016-09-22 19:13:23 +0800

    数据库表更新

--- autogenpapersystem.sql
+++ autogenpapersystem.sql
@@ -1,82 +1,202 @@
-﻿
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: AutoGenPaperSystem
+-- ------------------------------------------------------
+-- Server version	5.7.12-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `character`
+--
 
 DROP TABLE IF EXISTS `character`;
-
-
-CREATE TABLE `character` (                      //题目特点表
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `character` (                         /*题目特点表*/
   `Charact_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Charact_name` varchar(50) NOT NULL,                     
+  `Charact_name` varchar(50) NOT NULL,
   PRIMARY KEY (`Charact_ID`)
-)  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `character`
+--
+
+LOCK TABLES `character` WRITE;
+/*!40000 ALTER TABLE `character` DISABLE KEYS */;
+/*!40000 ALTER TABLE `character` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `difficulty`
+--
 
 DROP TABLE IF EXISTS `difficulty`;
-  
-  
-CREATE TABLE `difficulty` (                          //难度系数表
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `difficulty` (                       /*题目难度表*/
   `Difficulty_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Uplimit` double NOT NULL,                         //难度上限
-  `Lowlimit` double NOT NULL,                       //难度下限
+  `Uplimit` double NOT NULL,                      /*题目难度上限*/
+  `Lowlimit` double NOT NULL,                    /*题目难度下限*/
   PRIMARY KEY (`Difficulty_ID`)
-)  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-DROP TABLE IF EXISTS `grade`;                   
-  
-  
-CREATE TABLE `grade` (                          //年级表
+--
+-- Dumping data for table `difficulty`
+--
+
+LOCK TABLES `difficulty` WRITE;
+/*!40000 ALTER TABLE `difficulty` DISABLE KEYS */;
+/*!40000 ALTER TABLE `difficulty` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `grade`
+--
+
+DROP TABLE IF EXISTS `grade`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `grade` (                             /*年级表*/
   `Crade_ID` int(11) NOT NULL AUTO_INCREMENT,
   `Crade_name` varchar(10) NOT NULL,
   PRIMARY KEY (`Crade_ID`)
-)  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `grade`
+--
+
+LOCK TABLES `grade` WRITE;
+/*!40000 ALTER TABLE `grade` DISABLE KEYS */;
+/*!40000 ALTER TABLE `grade` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `knowledge`
+--
 
 DROP TABLE IF EXISTS `knowledge`;
-  
-  
-CREATE TABLE `knowledge` (                         //知识点表
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `knowledge` (                                   /*知识点表*/
   `Knowledge_ID` int(11) NOT NULL AUTO_INCREMENT,
   `Knowledge_name` varchar(100) NOT NULL,
-  `Subject_ID` int(11) NOT NULL,                   //对应学科ID
-  `Superior_ID` int(11) DEFAULT NULL,              //上级知识点ID
+  `Subject_ID` int(11) NOT NULL,                             /*所属课目ID*/
+  `Superior_ID` int(11) DEFAULT NULL,                      /*上级知识点ID*/
   PRIMARY KEY (`Knowledge_ID`)
-)  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `knowledge`
+--
+
+LOCK TABLES `knowledge` WRITE;
+/*!40000 ALTER TABLE `knowledge` DISABLE KEYS */;
+/*!40000 ALTER TABLE `knowledge` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questions`
+--
 
 DROP TABLE IF EXISTS `questions`;
-  
-  
-CREATE TABLE `questions` (                        //题目表
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `questions` (                                /*题目表*/
   `Questions_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Content` varchar(200) NOT NULL,                 //题目内容
-  `Subject_ID` int(11) NOT NULL,                   //对应学科ID
-  `Knowledge_ID1` int(11) DEFAULT NULL,            //对应知识点1
-  `Knowledge_ID2` int(11) DEFAULT NULL,            //对应知识点2
-  `Knowledge_ID3` int(11) DEFAULT NULL,            //对应知识点3
-  `Knowledge_ID4` int(11) DEFAULT NULL,            //对应知识点4
-  `Type_ID` int(11) NOT NULL,                      //对应题型
-  `Difficulty_ID` int(11) NOT NULL,                //对应难度
-  `Charact_ID` int(11) NOT NULL,                   //对应题目特点
-  `Isdelete` tinyint(1) NOT NULL DEFAULT '0',      //状态
+  `Content` varchar(200) NOT NULL,                       /*具体内容*/
+  `Subject_ID` int(11) NOT NULL,                         /*所属课目ID*/
+  `Knowledge_ID1` int(11) DEFAULT NULL,                  /*所属知识点1ID*/
+  `Knowledge_ID2` int(11) DEFAULT NULL,                  /*所属知识点2ID*/
+  `Knowledge_ID3` int(11) DEFAULT NULL,                  /*所属知识点3ID*/
+  `Knowledge_ID4` int(11) DEFAULT NULL,                  /*所属知识点4ID*/
+  `Type_ID` int(11) NOT NULL,                             /*所属类型ID*/
+  `Difficulty_ID` int(11) NOT NULL,                      /*所属难度ID*/
+  `Charact_ID` int(11) NOT NULL,                          /*所属题目特点ID*/
+  `Isdelete` tinyint(1) NOT NULL DEFAULT '0',             /*状态*/
   PRIMARY KEY (`Questions_ID`)
-)  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `questions`
+--
+
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subject`
+--
 
 DROP TABLE IF EXISTS `subject`;
-  
-  
-CREATE TABLE `subject` (                        //学科表
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `subject` (                                     /*课目表*/
   `Subject_ID` int(11) NOT NULL AUTO_INCREMENT,
   `Subject_name` varchar(20) NOT NULL,
-  `Grade_ID` int(11) NOT NULL,                   //对应年级
+  `Grade_ID` int(11) NOT NULL,                              /*所属年级ID*/
   PRIMARY KEY (`Subject_ID`)
-)  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `subject`
+--
+
+LOCK TABLES `subject` WRITE;
+/*!40000 ALTER TABLE `subject` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subject` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `type`
+--
 
 DROP TABLE IF EXISTS `type`;
-  
-  
-CREATE TABLE `type` (                       //题型表
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `type` (                                 /*题型表*/
   `Type_ID` int(11) NOT NULL AUTO_INCREMENT,
   `Type_name` varchar(20) NOT NULL,
-  `Subject_ID` int(11) NOT NULL,             //对应学科
+  `Subject_ID` int(11) NOT NULL,                      /*所属科目ID*/
   PRIMARY KEY (`Type_ID`)
-)  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `type`
+--
+
+LOCK TABLES `type` WRITE;
+/*!40000 ALTER TABLE `type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `type` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-09-22 17:56:07
