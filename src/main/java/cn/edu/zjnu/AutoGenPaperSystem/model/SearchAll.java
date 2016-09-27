fea4a08f3bfe4e96acfc7b9d0ea88a278dfd87f9
sgt110 <1067541968@qq.com>
2016-09-27 17:35:40 +0800

    添加diff字段

--- src/main/java/cn/edu/zjnu/AutoGenPaperSystem/model/SearchAll.java
+++ src/main/java/cn/edu/zjnu/AutoGenPaperSystem/model/SearchAll.java
@@ -9,16 +9,26 @@ public class SearchAll {
     private int char_id;
     private int types_id;
     private int know_id;
+    private int diff_id;
 
     public SearchAll() {
     }
 
-    public SearchAll(int grade_id, int sub_id, int char_id, int types_id, int know_id) {
+    public SearchAll(int grade_id, int sub_id, int char_id, int types_id, int know_id, int diff_id) {
         this.grade_id = grade_id;
         this.sub_id = sub_id;
         this.char_id = char_id;
         this.types_id = types_id;
         this.know_id = know_id;
+        this.diff_id = diff_id;
+    }
+
+    public int getDiff_id() {
+        return diff_id;
+    }
+
+    public void setDiff_id(int diff_id) {
+        this.diff_id = diff_id;
     }
 
     public int getGrade_id() {
